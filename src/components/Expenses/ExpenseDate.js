7fbf0b9ba2b4b6dda72c3d3c3350611c974c6f38
Mohammad <mohammadsorkhian@gmail.com>
2021-08-15 07:41:55 -0230

    created card wrapper

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,17 +1,18 @@
 import './ExpenseDate.css'
+import Card from '../UI/Card'
 
-function ExpenseDate(obj) {
+const ExpenseDate = (obj) => {
 
     const month = obj.date.toLocaleString('en-US', { month: 'long' })
     const year = obj.date.toLocaleString('en-US', { year: 'numeric' })
     const day = obj.date.toLocaleString('en-US', { day: '2-digit' })
 
     return (
-        <div className='expense-date'>
+        <Card className='expense-date'>
             <div className='expense-date__month'>{month}</div>
             <div className='expense-date__year'>{year}</div>
             <div className='expense-date__day'>{day}</div>
-        </div>
+        </Card>
     )
 }
 
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,17 @@
 import './ExpenseItems.css'
 import ExpenseDate from './ExpenseDate'
+import Card from '../UI/Card'
 
-function ExpenseItem(obj) {
+const ExpenseItem = (obj) => {
 
     return (
-        <div className='expense-item'>
+        <Card className='expense-item'>
             <ExpenseDate date={obj.date}></ExpenseDate>
             <div className='expense-item__description'>
                 <h2>{obj.title}</h2>
                 <div className='expense-item__price'>${obj.amount}</div>
             </div>
-        </div>
+        </Card>
     )
 }
 
